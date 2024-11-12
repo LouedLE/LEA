6b60158c9608d9f489c426650c18369bf0ab03f1
LouedLE <131865026+LouedLE@users.noreply.github.com>
2024-11-12 01:07:45 +0100

    Add files via upload

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // config.php : Connexion à la base de données
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "lea_web_creation";
+$servername = "leawebbloued.mysql.db";
+$username = "leawebbloued";
+$password = "Lea161204";
+$dbname = "leawebbloued";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
