abc1e0fc584f7d5ee0febbbe4bfa448ea7894c2b
LouedLE <131865026+LouedLE@users.noreply.github.com>
2025-09-02 17:03:33 +0200

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Configuration MySQL (WAMP local)
-return [
-  'db_host' => '127.0.0.1',
-  'db_name' => 'leawebbloued',
-  'db_user' => 'leawebbloued',
-  'db_pass' => 'Lea161204', // pas de mot de passe en local WAMP
-];
