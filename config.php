0f2c8ca0173b4c5de1b2679676418a78042514df
LouedLE <131865026+LouedLE@users.noreply.github.com>
2025-09-02 13:28:12 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+// Configuration MySQL (WAMP local)
+return [
+  'db_host' => '127.0.0.1',
+  'db_name' => 'leaweb',
+  'db_user' => 'root',
+  'db_pass' => '', // pas de mot de passe en local WAMP
+];
